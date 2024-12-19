686caab53f12d20d574af59282fcea81bf6c4853
wsb <767518216@qq.com>
2024-12-19 11:13:28 +0800

    leetcode中两个哈希表中关于异位词的题目

---
+++ src/main/java/com/wsb/leetcode/hashtable/canConstruct_383.java
@@ -0,0 +1,55 @@
+package com.wsb.leetcode.hashtable;
+
+public class canConstruct_383 {
+
+    public static void main(String[] args) {
+        String ransomNote = "aa";
+        String magazine = "aab";
+        System.out.println(canConstruct2(ransomNote, magazine));
+    }
+
+    private static boolean canConstruct1(String ransomNote, String magazine) {
+
+        if (ransomNote.length() > magazine.length()) return false;
+
+        StringBuilder stringBuilder = new StringBuilder(ransomNote);
+
+        for (int i = 0; i < magazine.length(); i++) {
+            for (int j = 0; j < stringBuilder.length(); j++) {
+                if (magazine.charAt(i) == stringBuilder.charAt(j)) {
+                    stringBuilder.deleteCharAt(j);
+                    break;
+                }
+            }
+        }
+
+        if (stringBuilder.length() == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private static boolean canConstruct2(String ransomNote, String magazine){
+
+        int[] record = new int[26];
+
+        if (ransomNote.length() > magazine.length()) return false;
+
+        for (char c : magazine.toCharArray()) {
+            record[c - 'a']++;
+        }
+
+        for (char c :ransomNote.toCharArray()) {
+            record[c - 'a']--;
+        }
+
+        for (int i : record) {
+            if (i < 0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/wsb/leetcode/hashtable/isAnagram_242.java
@@ -0,0 +1,52 @@
+package com.wsb.leetcode.hashtable;
+
+public class isAnagram_242 {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram2(s, t));
+
+    }
+
+    public static boolean isAnagram1(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        StringBuilder tBuilder = new StringBuilder(t);
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int index = tBuilder.indexOf(String.valueOf(c));
+            if (index == -1){
+                return false;
+            }else {
+                tBuilder.deleteCharAt(index);
+            }
+
+        }
+        return tBuilder.length() == 0;
+    }
+
+    public static boolean isAnagram2(String s, String t){
+
+        int[] record = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            record[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            record[t.charAt(i) - 'a']--;
+        }
+
+        for (int count :record){
+            if (count != 0) return false;
+        }
+
+        return true;
+    }
+
+
+
+}
