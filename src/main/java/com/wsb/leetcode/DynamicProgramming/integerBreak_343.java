12c169084c1dddee95d907748c66544bb0d46079
wsb <767518216@qq.com>
2025-03-18 11:06:04 +0800

    动态规划算法相关题目

---
+++ src/main/java/com/wsb/leetcode/DynamicProgramming/integerBreak_343.java
@@ -0,0 +1,14 @@
+package com.wsb.leetcode.DynamicProgramming;
+
+public class integerBreak_343 {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
